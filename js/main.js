8eed4df6f8aa952baf877453a5fcf025921e7ad6
navrajghimire <147842648+navrajghimire@users.noreply.github.com>
2024-03-21 10:57:53 -0600

    updated java script to make the hammenu open in mobile mode

--- js/main.js
+++ js/main.js
@@ -7,6 +7,17 @@
 /* For navbar section */
 
 
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
 
 var navbar = document.getElementById("navbar");
 window.onscroll = function() {scrollFunction()};
